86802fef971fff0004d45bf0d4a3bb2a6ea79321
Murilo Miranda <murilomiranda92@hotmail.com>
2019-12-05 21:50:22 -0300

    Update source.d

--- source.d
+++ source.d
@@ -1,19 +1,15 @@
 import identifying : visualizing;
 import reading : readTextImage;
 import std.stdio : writeln;
-import std.conv : to;
 
 void main()
 {
-    //A, B and J had their testing sets replaced
     bool individual = 0;
-    //foreach (i; 1 .. 26)
-        //readTextImage(individual, "B/" ~ to!string(i) ~ ".png", 'B');
-    /*writeln(readTextImage(individual, "alpha_Aakar.png"));
+    writeln(readTextImage(individual, "alpha_Aakar.png"));
     writeln(readTextImage(individual, "alpha_LiberationSerif.png"));
     writeln(readTextImage(individual, "alpha_Loma.png"));
     writeln(readTextImage(individual, "example_Suruma.png"));
     writeln(readTextImage(individual, "example_Ubuntu.png"));
-    foreach (i; 1 .. 26)
-        visualizing('A', i);*/
+    foreach (i; 0 .. 25)
+        visualizing(cast(char) ('A' + i));
 }
